932920b41a7ef5367cd564703101e7993617b009
Dominik Angerer <dominikangerer1@gmail.com>
2019-01-14 15:29:20 +0100

    adds new favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,76 +31,71 @@ module.exports = {
     link: [{
         rel: 'apple-touch-icon',
         sizes: '57x57',
-        href: 'https://img2.storyblok.com/57x57/f/40201/c55e612713/500x500_front_blue_transparent.png'
+        href: 'https://img2.storyblok.com/57x57/f/39898/515x515/3d1e05d1fc/storyblok-favicon.png'
       },
       {
         rel: 'apple-touch-icon',
         sizes: '60x60',
-        href: 'https://img2.storyblok.com/60x60/f/40201/c55e612713/500x500_front_blue_transparent.png'
+        href: 'https://img2.storyblok.com/60x60/f/39898/515x515/3d1e05d1fc/storyblok-favicon.png'
       },
       {
         rel: 'apple-touch-icon',
         sizes: '72x72',
-        href: 'https://img2.storyblok.com/72x72/f/40201/c55e612713/500x500_front_blue_transparent.png'
+        href: 'https://img2.storyblok.com/72x72/f/39898/515x515/3d1e05d1fc/storyblok-favicon.png'
       },
       {
         rel: 'apple-touch-icon',
         sizes: '76x76',
-        href: 'https://img2.storyblok.com/76x76/f/40201/c55e612713/500x500_front_blue_transparent.png'
+        href: 'https://img2.storyblok.com/76x76/f/39898/515x515/3d1e05d1fc/storyblok-favicon.png'
       },
       {
         rel: 'apple-touch-icon',
         sizes: '114x114',
-        href: 'https://img2.storyblok.com/114x114/f/40201/c55e612713/500x500_front_blue_transparent.png'
+        href: 'https://img2.storyblok.com/114x114/f/39898/515x515/3d1e05d1fc/storyblok-favicon.png'
       },
       {
         rel: 'apple-touch-icon',
         sizes: '120x120',
-        href: 'https://img2.storyblok.com/120x120/f/40201/c55e612713/500x500_front_blue_transparent.png'
+        href: 'https://img2.storyblok.com/120x120/f/39898/515x515/3d1e05d1fc/storyblok-favicon.png'
       },
       {
         rel: 'apple-touch-icon',
         sizes: '144x144',
-        href: 'https://img2.storyblok.com/144x144/f/40201/c55e612713/500x500_front_blue_transparent.png'
+        href: 'https://img2.storyblok.com/144x144/f/39898/515x515/3d1e05d1fc/storyblok-favicon.png'
       },
       {
         rel: 'apple-touch-icon',
         sizes: '152x152',
-        href: 'https://img2.storyblok.com/152x152/f/40201/c55e612713/500x500_front_blue_transparent.png'
+        href: 'https://img2.storyblok.com/152x152/f/39898/515x515/3d1e05d1fc/storyblok-favicon.png'
       },
       {
         rel: 'apple-touch-icon',
         sizes: '180x180',
-        href: 'https://img2.storyblok.com/180x180/f/40201/c55e612713/500x500_front_blue_transparent.png'
+        href: 'https://img2.storyblok.com/180x180/f/39898/515x515/3d1e05d1fc/storyblok-favicon.png'
       },
       {
         rel: 'icon',
         sizes: '32x32',
         type: 'image/png',
-        href: 'https://img2.storyblok.com/32x32/f/40201/c55e612713/500x500_front_blue_transparent.png'
+        href: 'https://img2.storyblok.com/32x32/f/39898/515x515/3d1e05d1fc/storyblok-favicon.png'
       },
       {
         rel: 'icon',
         sizes: '192x192',
         type: 'image/png',
-        href: 'https://img2.storyblok.com/192x192/f/40201/c55e612713/500x500_front_blue_transparent.png'
+        href: 'https://img2.storyblok.com/192x192/f/39898/515x515/3d1e05d1fc/storyblok-favicon.png'
       },
       {
         rel: 'icon',
         sizes: '96x96',
         type: 'image/png',
-        href: 'https://img2.storyblok.com/96x96/f/40201/c55e612713/500x500_front_blue_transparent.png'
+        href: 'https://img2.storyblok.com/96x96/f/39898/515x515/3d1e05d1fc/storyblok-favicon.png'
       },
       {
         rel: 'icon',
         sizes: '16x16',
         type: 'image/png',
-        href: 'https://img2.storyblok.com/16x16/f/40201/c55e612713/500x500_front_blue_transparent.png'
-      },
-      {
-        rel: 'mask-icon',
-        href: 'https://app.storyblok.com/images/safari-pinned-tab.svg',
-        color: '#5bbad5'
+        href: 'https://img2.storyblok.com/16x16/f/39898/515x515/3d1e05d1fc/storyblok-favicon.png'
       }
     ]
   },
@@ -139,8 +134,12 @@ module.exports = {
       let enrichedRoutes = []
       routes.forEach(route => {
         const parts = route.split('/')
-        const lang = parts[1]
-        const origin = parts[3]
+        let lang = parts[1]
+        let origin = parts[3]
+        if (lang == 'docs') {
+          lang = 'en'
+          origin = parts[2]
+        }
 
         enrichedRoutes.push({
           route: route,
