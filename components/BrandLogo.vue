f5f5b1618601da743acf292007a0dd9ee449ffd3
Dominik Angerer <dominikangerer1@gmail.com>
2019-01-15 11:23:05 +0100

    better label for logo

--- components/BrandLogo.vue
+++ components/BrandLogo.vue
@@ -1,5 +1,5 @@
 <template>
-  <a href="/" name="Back to startpage" class="brand-logo">
+  <a href="/" aria-label="Back to the Storyblok startpage" class="brand-logo">
     <svg viewBox="0 0 482 103" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
       <g id="storyblok-logo" fill-rule="nonzero">
         <g fill="#09B3AF">
