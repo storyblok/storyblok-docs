11396a8090499310c74b9fbb7470a6a8cf1701cf
Dominik Angerer <dominikangerer1@gmail.com>
2019-01-17 08:31:20 +0100

    reduce custom component creation

--- components/MethodExample.vue
+++ components/MethodExample.vue
@@ -19,18 +19,9 @@ export default {
       return this.method.path.split('/').join('-') + '-example';
     }
   },
-  components: {
-    RequestExample
-  },
   created() {
     Vue.component(this.exampleComponentName, {
       template: `<div>${this.method.example}</div>`,
-        methods: {
-        // formats date to YYYY-MM-DD HH:MM
-        formatDate(date) {
-          return date.getFullYear().toString() + "-"+((date.getMonth()+1).toString().length==2?(date.getMonth()+1).toString():"0"+(date.getMonth()+1).toString())+"-"+(date.getDate().toString().length==2?date.getDate().toString():"0"+date.getDate().toString())+" "+(date.getHours().toString().length==2?date.getHours().toString():"0"+date.getHours().toString())+":"+((parseInt(date.getMinutes()/5)*5).toString().length==2?(parseInt(date.getMinutes()/5)*5).toString():"0"+(parseInt(date.getMinutes()/5)*5).toString())
-        }
-      },
       components: {
         RequestExample
       }
