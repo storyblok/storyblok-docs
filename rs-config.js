3ce7482f33db957d76866cedecedfa0a52fd6882
Alexander Feiglstorfer <af@storyblok.com>
2019-02-04 10:54:07 +0100

    Changes asset folde

--- rs-config.js
+++ rs-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   assetInputFolder: `${__dirname}/dist/_nuxt`,
   contentInputFolder: `${__dirname}/dist/`,
-  assetOutputFolder:`${__dirname}/deploy/views/assets/_nuxt`,
+  assetOutputFolder:`${__dirname}/deploy/_nuxt`,
   contentOutputFolder: `${__dirname}/deploy/views/components/gnd/`,
   routerFile: `${__dirname}/deploy/views/docs-router.liquid`,
   spaceId: 51376
