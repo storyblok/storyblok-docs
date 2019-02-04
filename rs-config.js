e167fab74fdb48d9968d484af520685a7dd88480
Dominik Angerer <dominikangerer1@gmail.com>
2019-02-04 09:13:11 +0100

    adds rs-config

---
+++ rs-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  assetInputFolder: `${__dirname}/dist/_nuxt`,
+  contentInputFolder: `${__dirname}/dist/`,
+  assetOutputFolder:`${__dirname}/deploy/views/assets/_nuxt`,
+  contentOutputFolder: `${__dirname}/deploy/views/components/gnd/`,
+  routerFile: `${__dirname}/deploy/views/docs-router.liquid`,
+  spaceId: 51376
+}
